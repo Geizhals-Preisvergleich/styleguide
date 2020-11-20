5ebad344c66e10ef2e86e737c8bc915b2be309dd
Martin Grubinger <martin.grubinger@geizhals.at>
2020-11-20 12:32:21 +0100

    add badge page and component

---
+++ src/geizhals-components/Badge.svelte
@@ -0,0 +1,15 @@
+<script>
+  import PVBase from './../components/PVBase.svelte'
+</script>
+
+<style>
+  .listview__label{align-self:baseline;display:inline-block;padding:.125rem .25rem;margin:0 .25rem .25rem 0;font-size:.6rem;}
+  .listview__label--variant{background:#059;color:#fdfdfd;text-transform:uppercase;font-weight:700;}
+</style>
+
+
+<PVBase>
+  <span class="listview__label listview__label--variant">
+    Varianten
+  </span>
+</PVBase>
